fb9253d9254dbcffdc53fd84240a242cee0c1140
Otaxonov <Otaxonov_98@mail.ru>
2025-02-27 22:35:06 +0500

    Blog App

--- DFA/urls.py
+++ DFA/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.urls import re_path as url
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('Blog.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('Users.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
