5e0cff5a51ce1033f4cc59509e0826ba33f9fd53
Otaxonov <Otaxonov_98@mail.ru>
2025-02-27 22:26:11 +0500

    Blog App

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Users.models import Profile
 
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
--- Users/models.py
+++ Users/models.py
@@ -3,3 +3,14 @@ from django.db import models
 
 User = get_user_model()
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    image = models.ImageField(
+        default='user/profile/default.png',
+        upload_to='user/profile/images/'
+    )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
